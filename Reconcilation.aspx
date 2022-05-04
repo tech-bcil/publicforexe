6e7a5f462ddfc01f995c5c15929b96efb6196f6f
Kroria <83348320+Kroria@users.noreply.github.com>
2022-05-04 15:57:46 +0530

    Add files via upload

---
+++ Reconcilation.aspx
@@ -0,0 +1,153 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/WebPages/MobiVUEMaster.master" AutoEventWireup="true" CodeFile="Reconcilation.aspx.cs" Inherits="WebPages_Reconcilation" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+    <link href="../css/CalendarControl.css" type="text/css" rel="Stylesheet" />
+
+    <script language="javascript" src="../js/CalendarControl.js" type="text/javascript"></script>
+
+    <script language="javascript" type="text/javascript">
+        function ClearFields() {
+            document.getElementById('<%= lblErrorMsg.ClientID%>').value = "";
+            document.getElementById('<%= ddlSite.ClientID%>').value = "";
+           document.getElementById('<%= ddlFloor.ClientID%>').value = "";
+            document.getElementById('<%= ddlStore.ClientID%>').value = "";
+            document.getElementById('<%= txtRecId.ClientID%>').value = "";
+            document.getElementById('<%= txtFromDate.ClientID%>').value = "";
+            document.getElementById('<%= txtToDate.ClientID%>').value = "";
+        }
+        function ShowErrMsg(msg) {
+            document.getElementById('<%= lblErrorMsg.ClientID%>').innerHTML = msg.toString();
+        }
+        function ShowUnAuthorisedMsg() {
+            document.getElementById('<%= lblErrorMsg.ClientID%>').innerHTML = 'Please Note : You are not authorised to execute this operation!';
+        }
+    </script>
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <div id="clear">
+    </div>
+    <div id="wrapper">
+        <div id="pageTitle">
+            ASSET RECONCILIATION
+        </div>
+    </div>
+    <div id="wrapper1">
+        <table id="Table1" runat="server" cellspacing="10" style="width: 100%" align="center">
+            <tr>
+                <td colspan="4">
+                    <table id="Table2" runat="server" style="width: 100%;" cellspacing="10" align="center">                      
+                        
+                        <tr>                                                  
+                            <td style="text-align: left" colspan="3">
+                                  <asp:Button ID="btnStart" runat="server"  Text="Initiate ReConcilation"  CssClass="button"
+                                            TabIndex="26" Width="150px" ToolTip="Start ReConcile" style="margin-bottom:20px;"
+                                       onclick="btnStart_Click"   />
+                                 <asp:Button ID="btnStop" runat="server"  Text="Stop ReConcilation"  CssClass="button"
+                                            TabIndex="26" Width="150px" ToolTip="Stop ReConcile" style="margin-bottom:20px;"
+                                     onclick="btnStop_Click"     />                              
+                            </td>
+                        </tr>
+                        <tr>
+                             <td style="text-align: left">
+                                        <asp:Label ID="Label2" runat="server" Text="Site Location :" CssClass="label"></asp:Label>
+                                  </td>
+                                  <td style="text-align: left">
+                                  <asp:DropDownList runat="server" ID="ddlSite" CssClass="dropdownlist" Width="200" AutoPostBack="true" OnSelectedIndexChanged="ddlSite_SelectedIndexChanged" >
+                                        </asp:DropDownList>
+                                    </td>
+                                  
+                                    <td style="text-align: left">
+                                        <asp:Label ID="Label4" runat="server" Text="Floor :" CssClass="label"></asp:Label>
+                                        </td>
+                                  <td style="text-align: left">
+                                         <asp:DropDownList runat="server" ID="ddlFloor" CssClass="dropdownlist"  Width="200"  AutoPostBack="true"  ToolTip="Select Floor" OnSelectedIndexChanged="ddlFloor_SelectedIndexChanged"></asp:DropDownList>
+                                    </td>
+                            </tr>
+                         <tr>
+                             <td style="text-align: left">
+                                        <asp:Label ID="Label1" runat="server" Text="Store :" CssClass="label"></asp:Label>
+                                 </td>
+                                  <td style="text-align: left">
+                                         <asp:DropDownList runat="server" ID="ddlStore" CssClass="dropdownlist"  Width="200" OnSelectedIndexChanged="ddlStore_SelectedIndexChanged" ToolTip="Select Store"></asp:DropDownList>
+                                    </td>                           
+                       
+                        </tr>
+                         <tr Visible="false">
+                             <td style="text-align: left">
+                                 <asp:Label ID="Label3" runat="server" Text="Reconcilation Id :" CssClass="label"></asp:Label>
+                                 </td>
+                                  <td style="text-align: left">
+                                 <asp:TextBox runat="server" ID="txtRecId" Width="200" ToolTip="Enter Reconcilation ID"></asp:TextBox>
+                             </td>
+                             <td style="text-align: left">
+                                 <asp:Label ID="Label5" runat="server" Text="From Date :" CssClass="label"></asp:Label>
+                                 </td>
+                                  <td style="text-align: left">
+                                 <asp:TextBox ID="txtFromDate" runat="server" autocomplete="off" CssClass="textbox" MaxLength="50" 
+                                     ToolTip="Enter From Date" Width="200px" onfocus="showCalendarControl(this);"></asp:TextBox>
+                            </td>
+                             <td style="text-align: left">
+                                 <asp:Label ID="Label6" runat="server" Text="To Date :" CssClass="label"></asp:Label>
+                                 </td>
+                                  <td style="text-align: left">
+                                 <asp:TextBox ID="txtToDate" runat="server" autocomplete="off" CssClass="textbox" MaxLength="50" 
+                                     ToolTip="Enter To Date" Width="200px" onfocus="showCalendarControl(this);"></asp:TextBox>
+                            </td>
+                             </tr>
+                        <tr>
+                             <td style="text-align: left">
+                             <asp:Button ID="btnSearch" runat="server"  Text="Search"  CssClass="button" OnClick="btnSearch_Click"
+                                             Width="150px" ToolTip="Search" />  
+                                </td>
+                             <td style="text-align: left">
+                            <asp:Button ID="btnClear" runat="server"  Text="Clear"  CssClass="button"  OnClientClick="ClearFields();"
+                                  onclick="btnClear_Click"           Width="150px" ToolTip="Search" />  
+                                </td>
+                        </tr>
+                        <tr>
+                            <td style="text-align: left" colspan="6">
+                                <table id="tblAssets" runat="server" cellspacing="10" width="100%" 
+                                    align="center">
+                                    <tr style="border: 2px double #006600;">
+                                        <td style="vertical-align: top">
+                                            <asp:GridView ID="gvCodes" runat="server" AllowPaging="True" OnPageIndexChanging="gvCodes_PageIndexChanging"
+                                                ShowFooter="false" CssClass="mGrid" PagerStyle-CssClass="pgr" AlternatingRowStyle-CssClass="alt"
+                                                PageSize="50" >
+                                             
+                                                <PagerStyle CssClass="pgr"></PagerStyle>
+                                                <AlternatingRowStyle CssClass="alt"></AlternatingRowStyle>
+                                            </asp:GridView>
+                                            <asp:GridView ID="gvCodesFacilities" runat="server" AllowPaging="True" OnPageIndexChanging="gvCodesFacilities_PageIndexChanging"
+                                                ShowFooter="false" CssClass="mGrid" PagerStyle-CssClass="pgr" AlternatingRowStyle-CssClass="alt"
+                                                PageSize="50" >
+                                             
+                                                <PagerStyle CssClass="pgr"></PagerStyle>
+                                                <AlternatingRowStyle CssClass="alt"></AlternatingRowStyle>
+                                            </asp:GridView>
+                                        </td>                                   
+                                    </tr>                                    
+                                    
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="6">
+                                <asp:Label ID="lblErrorMsg" Font-Bold="true" CssClass="ErrorLabel" runat="server"></asp:Label>
+                            </td>
+                        </tr>
+                        <tr>
+                                        <td colspan="2" align="left">
+                                            <asp:ImageButton ID="btnExport" runat="server"  Enabled="true" ToolTip="Export assets list into excel file"
+                                                ImageUrl="~/images/Excel-icon (2).png" CausesValidation="false" 
+                                             onclick="btnExport_Click"   />
+                                        </td>
+                                    </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </div>
+</asp:Content>
+
+
---
+++ Reconcilation.aspx.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.IO;
+using MobiVUE_ATS.DAL;
+
+public partial class WebPages_Reconcilation : System.Web.UI.Page
+{
+    AssetReconcilation_DAL oDAL;
+    public WebPages_Reconcilation()
+    {
+
+    }
+    ~WebPages_Reconcilation()
+    {
+        oDAL = null;
+    }
+
+    #region PAGE EVENTS
+    /// <summary>
+    /// Navigates to session expired page in case of user logs off/session expired.
+    /// </summary>
+    protected void Page_Init(object sender, EventArgs e)
+    {
+        if (Session["CURRENTUSER"] == null)
+        {
+            Server.Transfer("SessionExpired.aspx");
+        }
+        oDAL = new AssetReconcilation_DAL(Session["DATABASE"].ToString());
+    }
+
+    /// <summary>
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            if (!IsPostBack)
+            {
+                btnStart.Enabled = false;
+                btnStop.Enabled = false;
+                gvCodes.Visible = false;
+                gvCodesFacilities.Visible = false;
+                HttpContext.Current.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                HttpContext.Current.Response.Cache.SetAllowResponseInBrowserHistory(false);
+                string _strRights = clsGeneral.GetRights("ASSET_RECONCILIATION", (DataTable)Session["UserRights"]);
+                clsGeneral._strRights = _strRights.Split('^');
+                clsGeneral.LogUserOperationToLogFile(Session["CURRENTUSER"].ToString(), Session["COMP_NAME"].ToString(), "ASSET_RECONCILIATION");
+                if (clsGeneral._strRights[0] == "0")
+                {
+                    Response.Redirect("UnauthorizedUser.aspx", false);
+                }
+                GetSiteDetails();
+                
+            }
+        }
+        catch (Exception ex)
+        {
+            HandleExceptions(ex);
+        }
+    }
+    #endregion
+
+    protected void btnStart_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            gvCodes.DataSource = null;
+            gvCodes.DataBind();
+            if(ddlSite.SelectedIndex == 0 || ddlFloor.SelectedIndex == 0)
+            {
+                ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "ShowErrMsg", "ShowErrMsg('Please select Location and Floor.');", true);
+                return;
+            }
+	        string Site = ddlSite.SelectedValue == "" ? null : ddlSite.SelectedValue;
+            string Floor = ddlFloor.SelectedValue == "" ? null : ddlFloor.SelectedValue;
+            string Store = ddlStore.SelectedValue == "" ? null : ddlStore.SelectedValue;
+            DataTable dc = oDAL.ReconcilationData("", Site, Floor, Store, "", "", Session["CURRENTUSER"].ToString(), "Validate", Session["COMPANY"].ToString());
+            if (dc.Rows.Count > 0)
+            {
+                ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "ShowErrMsg", "ShowErrMsg('Please Note : Reconciliation was already initiated.');", true);
+                return;
+            }
+            DataTable dt = oDAL.ReconcilationData("",Site, Floor, Store, "", "", Session["CURRENTUSER"].ToString(), "Generate", Session["COMPANY"].ToString());
+            if (dt.Rows.Count > 0)
+            {
+                if (dt.Rows[0]["ReturnId"].ToString() == "0")
+                {
+                    btnStop.Enabled = true;
+                    btnStart.Enabled = false;
+                    ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "ShowErrMsg", "ShowErrMsg('Please Note : Reconciliation process started successfully.');", true);                    
+                }
+                else
+                {
+                    ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "ShowErrMsg", "ShowErrMsg('Please Note : Reconciliation not initiated.');", true);
+                    return;
+                }
+            }
+            else
+            {
+                ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "ShowErrMsg", "ShowErrMsg('Please Note : Reconciliation not initiated.');", true);
+                return;
+            }
+        }
+        catch (Exception ex)
+        {
+            HandleExceptions(ex);
+        }
+    }
+
+    #region PRIVATE FUNCTIONS
+    /// <summary>
+    /// Cathes exception for the entier page operations.
+    /// </summary>
+    public void HandleExceptions(Exception ex)
+    {
+        clsGeneral.LogErrorToLogFile(ex, "Reconciliation");
+        if (!ex.Message.ToString().Contains("Thread was being aborted."))
+        {
+            clsGeneral.ErrMsg = ex.Message.ToString(); try { string[] arrErr = ex.Message.ToString().Split('\n'); Session["ErrMsg"] = arrErr[0].ToString().Trim(); }
+            catch { }
+            Server.Transfer("Error.aspx");
+        }
+    }
+    private void GetSiteDetails()
+    {
+        DataTable dt = new DataTable();
+        ddlSite.DataSource = null;
+        dt = oDAL.GetSite(Session["COMPANY"].ToString());
+        ddlSite.DataSource = dt;
+        ddlSite.DataTextField = "SITE_CODE";
+        ddlSite.DataValueField = "SITE_CODE";
+        ddlSite.DataBind();
+        ddlSite.Items.Insert(0, "-- Select --");
+    }
+    
+    private void GetFloorDropdown(string SiteCode)
+    {
+        DataTable dt = new DataTable();
+        ddlFloor.DataSource = null;
+        dt = oDAL.GetFloor(SiteCode, Session["COMPANY"].ToString());
+        ddlFloor.DataSource = dt;
+        ddlFloor.DataTextField = "FLOOR_CODE";
+        ddlFloor.DataValueField = "FLOOR_CODE";
+        ddlFloor.DataBind();
+        ddlFloor.Items.Insert(0, "-- Select --");
+    }
+    private void GetStoreDropdown(string Site, string Floor)
+    {
+        DataTable dt = new DataTable();
+        ddlStore.DataSource = null;
+        dt = oDAL.GetStore(Site, Floor, Session["COMPANY"].ToString());
+        ddlStore.DataSource = dt;
+        ddlStore.DataTextField = "STORE_CODE";
+        ddlStore.DataValueField = "STORE_CODE";
+        ddlStore.DataBind();
+        ddlStore.Items.Insert(0, "-- Select --");
+    }
+    private void LoadGridData()
+    {
+        string Recid = txtRecId.Text.Trim() == "" ? null : txtRecId.Text.Trim();
+        string Site = ddlSite.SelectedValue == "" ? null : ddlSite.SelectedValue;
+        string Floor = ddlFloor.SelectedValue == "" ? null : ddlFloor.SelectedValue;
+        string Store = ddlStore.SelectedValue == "" ? null : ddlStore.SelectedValue;
+        string FromDate = txtFromDate.Text.Trim() == "" ? null : txtFromDate.Text.Trim();
+        string ToDate = txtToDate.Text.Trim() == "" ? null : txtToDate.Text.Trim();
+        gvCodes.DataSource = null;
+        gvCodes.DataBind();
+        DataTable dt =  oDAL.ReconcilationData(Recid, Site, Floor, Store, FromDate, ToDate, Session["CURRENTUSER"].ToString(), "GetData", Session["COMPANY"].ToString());
+        Session["ReconData"] = dt;
+        gvCodes.DataSource = dt;
+        gvCodes.DataBind();
+        if (Session["COMPANY"].ToString() == "IT")
+        {
+            gvCodes.Visible = true;
+            gvCodes.DataSource = dt;
+            gvCodes.DataBind();
+        }
+        else
+        {
+            gvCodesFacilities.Visible = true;
+            gvCodesFacilities.DataSource = dt;
+            gvCodesFacilities.DataBind();
+        }
+    }
+    #endregion
+
+    protected void ddlSite_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        try
+        {
+            GetFloorDropdown(ddlSite.SelectedValue);
+        }
+        catch (Exception ex)
+        {
+            HandleExceptions(ex);
+        }        
+    }
+
+    protected void ddlFloor_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        try
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = false;
+            GetStoreDropdown(ddlSite.SelectedValue, ddlFloor.SelectedValue);
+            string Site = ddlSite.SelectedValue == "" ? null : ddlSite.SelectedValue;
+            string Floor = ddlFloor.SelectedValue == "" ? null : ddlFloor.SelectedValue;
+            string Store = ddlStore.SelectedValue == "" ? null : ddlStore.SelectedValue;
+            DataTable dt = oDAL.ReconcilationData("", Site, Floor, Store, "", "", Session["CURRENTUSER"].ToString(), "Validate", Session["COMPANY"].ToString());
+            if (dt.Rows.Count > 0)
+            {
+                btnStop.Enabled = true;
+            }
+            else
+            {
+                btnStart.Enabled = true;
+            }
+        }
+        catch (Exception ex)
+        {
+            HandleExceptions(ex);
+        }
+    }
+
+    protected void btnSearch_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            LoadGridData();
+        }
+        catch (Exception ex)
+        {
+            HandleExceptions(ex);
+        }
+    }
+
+    protected void gvCodes_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        try
+        {
+            gvCodes.PageIndex = e.NewPageIndex;
+            LoadGridData();
+        }
+        catch (Exception ex)
+        { HandleExceptions(ex); }
+    }
+
+    protected void btnStop_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            gvCodes.DataSource = null;
+            gvCodes.DataBind();
+            if (ddlSite.SelectedIndex == 0 || ddlFloor.SelectedIndex == 0)
+            {
+                ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "ShowErrMsg", "ShowErrMsg('Please select Location and Floor.');", true);
+                return;
+            }
+            string Site = ddlSite.SelectedValue == "" ? null : ddlSite.SelectedValue;
+            string Floor = ddlFloor.SelectedValue == "" ? null : ddlFloor.SelectedValue;
+            string Store = ddlStore.SelectedValue == "" ? null : ddlStore.SelectedValue;
+            DataTable dc = oDAL.ReconcilationData("", Site, Floor, Store, "", "", Session["CURRENTUSER"].ToString(), "Validate", Session["COMPANY"].ToString());
+            if (dc.Rows.Count == 0)
+            {
+                ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "ShowErrMsg", "ShowErrMsg('Please Note : Reconciliation was already stopped.');", true);
+                return;
+            }
+            DataTable dt = oDAL.ReconcilationData("", Site, Floor, Store, "", "", Session["CURRENTUSER"].ToString(), "Stop", Session["COMPANY"].ToString());
+            if (dt.Rows.Count > 0)
+            {
+                if (dt.Rows[0]["ReturnId"].ToString() == "0")
+                {
+                    btnStart.Enabled = true;
+                    btnStop.Enabled = false;
+                    ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "ShowErrMsg", "ShowErrMsg('Please Note : Reconciliation process stopped successfully.');", true);
+                }
+                else
+                {
+                    ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "ShowErrMsg", "ShowErrMsg('Please Note : Reconciliation not initiated.');", true);
+                    return;
+                }
+            }
+            else
+            {
+                ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "ShowErrMsg", "ShowErrMsg('Please Note : Reconciliation not initiated.');", true);
+                return;
+            }
+        }
+        catch (Exception ex)
+        {
+            HandleExceptions(ex);
+        }
+    }
+
+    protected void btnClear_Click(object sender, EventArgs e)
+    {
+        gvCodes.DataSource = null;
+        gvCodes.DataBind();
+        ddlFloor.SelectedIndex = 0;
+        ddlSite.SelectedIndex = 0;
+        ddlStore.SelectedIndex = 0;
+        btnStop.Enabled = false;
+        btnStart.Enabled = false;
+    }
+
+    protected void btnExport_Click(object sender, ImageClickEventArgs e)
+    {
+        try
+        {
+            if (Session["COMPANY"].ToString() == "IT" && gvCodes.Rows.Count == 0)
+            {
+                ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "ShowErrMsg", "ShowErrMsg('Please Note : There is no data for being exported.');", true);
+                return;
+            }
+            if (Session["COMPANY"].ToString() != "IT" && gvCodesFacilities.Rows.Count == 0)
+            {
+                ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "ShowErrMsg", "ShowErrMsg('Please Note : There is no data for being exported.');", true);
+                return;
+            }
+            DataTable dt1 = new DataTable();
+            if (Session["ReconData"] != null)
+            {
+                dt1 = (DataTable)Session["ReconData"];
+            }
+            if (dt1.Rows.Count > 0)
+            {
+                dt1.TableName = "ReconcilationData";
+                using (ClosedXML.Excel.XLWorkbook wb = new ClosedXML.Excel.XLWorkbook())
+                {
+                    wb.Worksheets.Add(dt1);
+                    wb.Style.Alignment.Horizontal = ClosedXML.Excel.XLAlignmentHorizontalValues.Center;
+                    wb.Style.Font.Bold = true;
+                    Response.Clear();
+                    Response.Buffer = true;
+                    Response.Charset = "";
+                    Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                    Response.AddHeader("content-disposition", "attachment;filename=ReconcilationDataReport" + DateTime.Now.ToString("yyyyMMddHHmm") + ".xlsx");
+
+                    using (MemoryStream MyMemoryStream = new MemoryStream())
+                    {
+                        wb.SaveAs(MyMemoryStream);
+                        MyMemoryStream.WriteTo(Response.OutputStream);
+                        Response.Flush();
+                        Response.End();
+                    }
+                }
+            }
+            else
+            {
+                ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "ShowErrMsg", "ShowErrMsg('Please Note : There is no data for being exported.');", true);
+                return;
+            }
+        }
+        catch (Exception ex)
+        {
+            HandleExceptions(ex);
+        }
+    }
+
+    protected void gvCodesFacilities_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        try
+        {
+            gvCodesFacilities.PageIndex = e.NewPageIndex;
+            LoadGridData();
+        }
+        catch (Exception ex)
+        { HandleExceptions(ex); }
+    }
+
+    protected void ddlStore_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        try
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = false;
+            string Site = ddlSite.SelectedValue == "" ? null : ddlSite.SelectedValue;
+            string Floor = ddlFloor.SelectedValue == "" ? null : ddlFloor.SelectedValue;
+            string Store = ddlStore.SelectedValue == "" ? null : ddlStore.SelectedValue;
+            DataTable dt = oDAL.ReconcilationData("", Site, Floor, Store, "", "", Session["CURRENTUSER"].ToString(), "Validate", Session["COMPANY"].ToString());
+            if (dt.Rows.Count > 0)
+            {
+                btnStop.Enabled = true;
+            }
+            else
+            {
+                btnStart.Enabled = true;
+            }
+        }
+        catch (Exception ex)
+        { HandleExceptions(ex); }
+    }
+}
